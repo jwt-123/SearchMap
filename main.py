ed60b29be3240e1e0d9f66e1f06462f741cf190c
jwt-123 <867491358@qq.com>
2022-03-14 17:26:08 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+import banner
+import reptile
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    banner.test()
+    reptile.test()
---
+++ reptile.py
@@ -0,0 +1,8 @@
+#请求资源，request
+#支持多线程和异步
+#写好接口，测试用例
+
+
+
+def test():
+    print("reptile")
